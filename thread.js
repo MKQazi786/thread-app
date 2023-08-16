f522ec02dad76759c539b43bb9a3fa1f50183418
Muhammad Khubaib <muhammadkhubaib492@gmail.com>
2023-08-16 19:05:27 +0500

    i have completed my thread auth

--- thread.js
+++ thread.js
@@ -1,48 +1,60 @@
-  const firebaseConfig = {
-    apiKey: "AIzaSyBBJ9weAlayMFFvTMBuN_HOChJYPuB1jEw",
-    authDomain: "real-time-thread-app.firebaseapp.com",
-    projectId: "real-time-thread-app",
-    storageBucket: "real-time-thread-app.appspot.com",
-    messagingSenderId: "991717444181",
-    appId: "1:991717444181:web:206453b4a012e48b41a2dc"
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const auth = firebase.auth();
+const firebaseConfig = {
+  apiKey: "AIzaSyBBJ9weAlayMFFvTMBuN_HOChJYPuB1jEw",
+  authDomain: "real-time-thread-app.firebaseapp.com",
+  projectId: "real-time-thread-app",
+  storageBucket: "real-time-thread-app.appspot.com",
+  messagingSenderId: "991717444181",
+  appId: "1:991717444181:web:206453b4a012e48b41a2dc"
+};
 
 
-  let email = document.getElementById("emailAddress")
-  let password = document.getElementById("Password")
 
-  window.signup = () => {
-      
-      firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-          // Signed in 
-          var user = userCredential.user;
-          // ...
-        })
-        .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // ..
-        });
-        
-}
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const database   = firebase.database();
 
-  firebase.auth().signInWithEmailAndPassword(email, password)
+
+window.signUp = (event) => {
+  
+  event.preventDefault()
+
+  let email = document.getElementById("emailAddress").value
+  let password = document.getElementById("Password").value
+  
+  firebase.auth().createUserWithEmailAndPassword(email, password)
   .then((userCredential) => {
-    // Signed in
+    // Signed in 
     var user = userCredential.user;
     // ...
   })
   .catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
+    // ..
   });
+  
+}
 
-  firebase.auth().signOut().then(() => {
-    // Sign-out successful.
-  }).catch((error) => {
-    // An error happened.
-  });
+window.logIn = (event) => {
+  
+  event.preventDefault()
+  let email = document.getElementById("emailAddress").value
+  let password = document.getElementById("Password").value
+  
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+
+}
+firebase.auth().signOut().then(() => {
+  // Sign-out successful.
+}).catch((error) => {
+  // An error happened.
+});
